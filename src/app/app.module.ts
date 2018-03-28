ae18e4e8d3f2ef2b302248056000c48c25b3cebf
PJ BELL <pj@PJs-MacBook-Pro.local>
2018-03-28 00:31:30 -0700

    my first message

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { DemoComponent } from './demo/demo.component';
+import { LandingComponent } from './landing/landing.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
+const appRoutes: Routes = [
+  { path: '', component: LandingComponent },
+  { path: 'demo', component: DemoComponent }
+];
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DemoComponent,
+    LandingComponent,
+    NavbarComponent
+  ],
+  imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: false } // <-- debugging purposes only
+    ),
+    BrowserModule,
+    NgbModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
